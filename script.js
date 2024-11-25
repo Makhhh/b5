553d77b40fd6445eddca04caa1f6f0276fb3a5ab
Makhhh <makhmarkhov@gmail.com>
2024-11-25 17:44:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+//            TASK 1
+
+// let san1 = +prompt("san bir engiz")
+// let san2 = +prompt("san eki engiz")
+
+// let m = azaitu(san1,san2)
+// let q = qosu(san1,san2)
+// let b = bolu(san1,san2)
+// let k = kobeitu(san1,san2)
+
+// function qosu(san1 , san2) {
+//     return san1 + san2
+// }
+// function azaitu(san1 , san2) {
+//     return san1 - san2
+// }
+// function bolu(san1 , san2) {
+//     return san1 / san2
+// }
+// function kobeitu(san1 , san2) {
+//     return san1 * san2
+// }
+// alert("eki san kosyndysy = " + q + '\n' + "eki san azaityndysy = " + m + '\n' + "eki san kobeityndysy = " + k + '\n' + "eki san bolyndysy = " + b )
+
+
+//                 TASK 3
+
+// while (true) {
+//     let san = 50;
+//     let tap = +prompt("Sandi Tap 0-100")
+    
+//     if (tap == san ) {
+//         alert("Krash sandi taptyn!")
+//         break;
+//     }
+//     else if (tap>=40 && tap<=49) {
+//         alert("Sizdin Sanynyz sal tomen")
+//     }
+//     else if (tap>=51 && tap<=60){
+//         alert("Sizdin sanynyz sal jogary")
+//     }
+//     else {
+//         alert("Sanynyz kerekten alis!")
+//     }
+// }
+
+
+
+//             TASK 2
+
+// let san = +prompt("Kansha zat alasiz?")
+// let baga = +prompt("Kansha tengege zat alasiz?")
+
+    
+
+// let sum = 0;
+
+// if (baga>=5000 && baga<=10000) {
+//     sum = baga - (baga/10)
+// }
+// else if (baga>=10000 && baga<=20000) {
+//     sum = sum = baga - (baga/20) 
+// }
+// else if (baga>=20000) {
+//     sum = sum = baga - (baga/30)
+// }
+// else{
+//    alert("Sizge skidka karakmagan")
+// }
+
+// alert("Sizdin baga skidkamen = " + sum)
+
+
+
+
+
+//       BONUS TASK
+
+// let num1 = 10;
+// let num2 = 20;
+
